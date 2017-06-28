c08dbb2771de1d7983d613c30b5e8635c334bd71
Dieter Flick <dflick@pivotal.io>
2017-06-28 22:35:46 +0200

    added service info creator for scs user provided service instances

---
+++ spring-cloud-services-cloudfoundry-connector/src/main/java/io/pivotal/spring/cloud/cloudfoundry/UpsiEurekaServiceInfoCreator.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.pivotal.spring.cloud.cloudfoundry;
+
+import java.util.Map;
+
+import io.pivotal.spring.cloud.service.common.EurekaServiceInfo;
+
+import org.springframework.cloud.cloudfoundry.CloudFoundryServiceInfoCreator;
+import org.springframework.cloud.cloudfoundry.Tags;
+
+/**
+ * Service info creator for Eureka services
+ *
+ * @author Chris Schaefer
+ */
+public class UpsiEurekaServiceInfoCreator extends CloudFoundryServiceInfoCreator<EurekaServiceInfo> {
+	private static final String CREDENTIALS_ID_KEY = "name";
+	private static final String EUREKA_SERVICE_TAG_NAME = "eureka";
+
+	public UpsiEurekaServiceInfoCreator() {
+		super(new Tags(EUREKA_SERVICE_TAG_NAME));
+	}
+
+	@Override
+	public EurekaServiceInfo createServiceInfo(Map<String, Object> serviceData) {
+		String id = (String) serviceData.get(CREDENTIALS_ID_KEY);
+		Map<String, Object> credentials = getCredentials(serviceData);
+		String uri = getUriFromCredentials(credentials);
+		
+		String clientId = getStringFromCredentials(credentials, "client_id");
+		String clientSecret = getStringFromCredentials(credentials, "client_secret");
+		String accessTokenUri = getStringFromCredentials(credentials, "access_token_uri");
+		
+		return new EurekaServiceInfo(id, uri, clientId, clientSecret, accessTokenUri);
+	}
+
+	@Override
+	public boolean accept(Map<String, Object> serviceData) {
+		return ((String) serviceData.get("name")).startsWith("service-registry");
+	}
+}
---
+++ spring-cloud-services-cloudfoundry-connector/src/main/java/io/pivotal/spring/cloud/cloudfoundry/UpsiHystrixAmqpServiceInfoCreator.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.pivotal.spring.cloud.cloudfoundry;
+
+import java.util.List;
+import java.util.Map;
+
+import io.pivotal.spring.cloud.service.common.HystrixAmqpServiceInfo;
+
+import org.springframework.cloud.cloudfoundry.CloudFoundryServiceInfoCreator;
+import org.springframework.cloud.cloudfoundry.Tags;
+
+/**
+ * Service info creator for Hystrix AMQP services
+ *
+ * @author Scott Frederick
+ * @author Roy Clarkson
+ */
+public class UpsiHystrixAmqpServiceInfoCreator extends CloudFoundryServiceInfoCreator<HystrixAmqpServiceInfo> {
+
+	private static final String TAG_NAME = "hystrix-amqp";
+
+	private static final String CREDENTIALS_ID_KEY = "name";
+
+	private static final String URIS_ID_KEY = "uris";
+
+	private static final String SSL_ID_KEY = "ssl";
+
+	public static final String AMQP_CREDENTIALS_KEY = "amqp";
+
+	public UpsiHystrixAmqpServiceInfoCreator() {
+		super(new Tags(TAG_NAME));
+	}
+
+	@Override
+	public boolean accept(Map<String, Object> serviceData) {
+		return ((String) serviceData.get("name")).startsWith("circuit-breaker");
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public HystrixAmqpServiceInfo createServiceInfo(Map<String, Object> serviceData) {
+		String id = (String) serviceData.get(CREDENTIALS_ID_KEY);
+		Map<String, Object> credentials = getCredentials(serviceData);
+		Map<String, Object> amqpCredentials = (Map<String, Object>) credentials.get(AMQP_CREDENTIALS_KEY);
+		String uri = getUriFromCredentials(amqpCredentials);
+		List<String> uris = (List<String>) amqpCredentials.get(URIS_ID_KEY);
+		boolean sslEnabled = Boolean.valueOf(amqpCredentials.get(SSL_ID_KEY).toString());
+		return new HystrixAmqpServiceInfo(id, uri, uris, sslEnabled);
+	}
+}
